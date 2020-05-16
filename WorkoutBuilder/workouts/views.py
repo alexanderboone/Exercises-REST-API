67d4a27a099fcebe46a8d244595804dea47cf486
Alexander Boone <alboone@uw.edu>
2020-05-15 21:52:46 -0700

    Created MuscleGroup model and revised fields for Exercise and MuscleGroup models

--- WorkoutBuilder/workouts/views.py
+++ WorkoutBuilder/workouts/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. This is the workouts app index.")
-
-
-
+def home_view(request):
+    return render(request, "home.html", {})
