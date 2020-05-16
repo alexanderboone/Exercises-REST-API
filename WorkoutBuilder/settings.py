4484854579a2b7012218dc76e84f173aff55a31a
Alexander Boone <alboone@uw.edu>
2020-05-15 21:52:46 -0700

    Created MuscleGroup model and revised fields for Exercise and MuscleGroup models

--- WorkoutBuilder/settings.py
+++ WorkoutBuilder/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'workouts.apps.WorkoutsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'WorkoutBuilder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+mechanics_choices = [
+    ('Comp', 'Compound'),
+    ('Iso', 'Isolation'),
+    ('NA', 'Not Applicable')
+]
+
+ppl_choices = [
+    ('Push', 'Push'),
+    ('Pull', 'Pull'),
+    ('Legs', 'Legs')
+]
+
+class MuscleGroup(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=64)
+    muscle_group = models.ManyToManyField('MuscleGroup', related_name='exercises')
+    mechanics = models.CharField(
+        max_length=4,
+        choices=mechanics_choices,
+        default='NA'
+    )
+    ppl = models.CharField(
+        max_length=4,
+        choices=ppl_choices,
+        default='NA'
+    )
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
