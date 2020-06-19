c62cfcdc05b1902bc54e1588e7184255b7a5a68f
Alexander Boone <alboone@uw.edu>
2020-06-19 09:53:22 -0500

    added REST API endpoints for exercises and musclegroups

--- WorkoutBuilder/WorkoutBuilder/settings.py
+++ WorkoutBuilder/WorkoutBuilder/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'workouts.apps.WorkoutsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
