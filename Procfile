5cab49549946f6f497da901e5a1674a49c244fbf
Alexander Boone <alboone@uw.edu>
2020-08-31 16:19:25 -0700

    deleted unneeded files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn WorkoutBuilder.wsgi --log-file -
--- workouts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Exercise)
-admin.site.register(models.MuscleGroup)
